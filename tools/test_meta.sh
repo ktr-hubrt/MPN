a85ba21c52cb866d54bc3a55238394b8e6211d5c
Hubrt Hui <32117005+ktr-hubrt@users.noreply.github.com>
2021-05-24 20:04:59 +0800

    Update test_meta.sh

--- tools/test_meta.sh
+++ tools/test_meta.sh
@@ -2,4 +2,4 @@
 
 PYTHON=${PYTHON:-"python"}
 
-$PYTHON Test_meta.py --gpus $1 --dataset_path '../ano_pred_cvpr2018/Data/' --dataset_type $2 --model_dir 'exp/'$2'/'$3
+$PYTHON Test_meta.py --gpus $1 --dataset_path './Data/' --dataset_type $2 --model_dir 'exp/'$2'/'$3
