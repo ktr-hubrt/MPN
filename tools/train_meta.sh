2a4f0ad0e8eb62c08cc28420f172ff6b9233a83f
Hubrt Hui <32117005+ktr-hubrt@users.noreply.github.com>
2021-05-24 20:04:43 +0800

    Update train_meta.sh

--- tools/train_meta.sh
+++ tools/train_meta.sh
@@ -2,4 +2,4 @@
 
 PYTHON=${PYTHON:-"python"}
 
-$PYTHON Train_meta.py --gpus $1 --dataset_path '../ano_pred_cvpr2018/Data/' --dataset_type $2 --exp_dir $3
+$PYTHON Train_meta.py --gpus $1 --dataset_path './Data/' --dataset_type $2 --exp_dir $3
